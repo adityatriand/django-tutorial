6e7df21e68330f6ae76ed0f1ed67254243f83dfe
adityatriand <adityatriananda0@gmail.com>
2021-10-27 13:28:00 +0700

    try model, do modularity

--- mywebsite/belajar/views.py
+++ mywebsite/belajar/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def index(request):
+    pengalaman = models.PengalamanBelajar.objects.all()
     context = {
-        'judul':'Install Django'
+        'judul':'Install Django',
+        'belajars': pengalaman
     }
     return render(request, 'belajar/index.html', context)
