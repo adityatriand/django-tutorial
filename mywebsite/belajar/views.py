1bd9debd544349aa49e504896a6096c1cb5d43ea
adityatriand <adityatriananda0@gmail.com>
2021-10-27 09:32:12 +0700

    update tutorial

--- mywebsite/belajar/views.py
+++ mywebsite/belajar/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'belajar/index.html')
+    context = {
+        'judul':'Install Django'
+    }
+    return render(request, 'belajar/index.html', context)
--- mywebsite/mywebsite/views.py
+++ mywebsite/mywebsite/views.py
@@ -2,4 +2,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'judul':'Tutorial Django',
+    }
+    return render(request, 'index.html',context)
